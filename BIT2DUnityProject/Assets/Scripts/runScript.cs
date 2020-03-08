6ab19bf2c4f4be5ed14f2b797af35134e9730864
Terminal-Est <cstephen1982@gmail.com>
2020-03-08 19:39:26 +1100

    Added a test sprite animation
    Hey team, added a test zombie sprite animation to our project. Have a look! To make the zombie run, hit A.

---
+++ BIT2DUnityProject/Assets/Scripts/runScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class runScript : MonoBehaviour
+{
+
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKey(KeyCode.A))
+        {
+            animator.SetInteger("runInt", 1);
+        } else
+        {
+            animator.SetInteger("runInt", 0);
+        }
+
+
+    }
+}
